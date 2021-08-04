3edd8bc07f72d18e5d1eb8cda324a6c3f61aaebd
Dan Houghton <dan.houghton@gmail.com>
2021-08-04 22:35:20 +0100

    tidy up columns ready for first release (#13)
    * removed n_trailing as niche
    * moved info_rate first, and order by it--if requested it's a special use case
    * quote strings in min_value/max_value

--- macros/profile_cols.sql
+++ macros/profile_cols.sql
@@ -7,7 +7,7 @@
 {% endmacro %}
 
 {% macro profile_strings(relation) %}
-    {{ profile_cols(relation, col_is_string, n_empty=True, n_trailing=True, max_characters=True )}}
+    {{ profile_cols(relation, col_is_string, n_empty=True, max_characters=True )}}
 {% endmacro %}
 
 {% macro profile_numbers(relation) %}
@@ -39,9 +39,10 @@
     sample_n=0,
     rate_precision=4,
     data_type=True,
+    info_rate=False,
     n_null=True, null_rate=True,
-    n_distinct=True, distinct_rate=True, info_rate=False,
-    n_empty=False, n_trailing=False, max_characters=False,
+    n_distinct=True, distinct_rate=True,
+    n_empty=False, max_characters=False,
     min_value=True, max_value=True, avg_value=False,
     most_common_values=True
     ) %}
@@ -54,7 +55,8 @@
         order by random() 
         limit {{ sample_n }}
         {% endif %}
-    )
+    ),
+    results as (
 
     {%- for col in all_cols if cols is none 
         or (cols is sequence and col.name is in(cols)) 
@@ -66,6 +68,20 @@
 
         {%- if data_type %},
             '{{ col.data_type }}' as data_type
+        {% endif %}
+
+        {%- if info_rate %},
+            round((
+                with freq as (
+                    select count(*) as f
+                    from source
+                    group by {{ adapter.quote(col.name) }}
+                ),
+                n as (
+                    select sum(f) as n from freq
+                )
+                select -sum(f/n*ln(f/n)/ln(n)) from freq, n
+            ), {{ rate_precision }}) as info_rate
         {% endif %}
 
         {%- if n_null %},
@@ -113,34 +129,12 @@
             end as distinct_rate
         {% endif %}
 
-        {%- if info_rate %},
-            round((
-                with freq as (
-                    select count(*) as f
-                    from source
-                    group by {{ adapter.quote(col.name) }}
-                ),
-                n as (
-                    select sum(f) as n from freq
-                )
-                select -sum(f/n*ln(f/n)/ln(n)) from freq, n
-            ), {{ rate_precision }}) as info_rate
-        {% endif %}
-
         {%- if n_empty %},
             {%- if col.is_string() %}
             count(case when {{ adapter.quote(col.name) }} = '' then 1 end)
             {% else %}
             null::integer
             {% endif %} as n_empty
-        {% endif %}
-
-        {%- if n_trailing %},
-            {%- if col.is_string() %}
-            count(case when {{ adapter.quote(col.name) }} != rtrim({{ adapter.quote(col.name) }}) then 1 end)
-            {% else %}
-            null::integer
-            {% endif %} as n_trailing
         {% endif %}
 
         {%- if max_characters %},
@@ -161,7 +155,9 @@
             {%- if col.is_number() %}
             cast(min({{ adapter.quote(col.name) }}) as varchar)
             {% else %}
+            {% if col.is_string() %}'"' || {% endif -%}
             min(cast({{ adapter.quote(col.name) }} as varchar))
+            {%- if col.is_string() %} || '"'{% endif %}
             {% endif %} as min_value
         {% endif %}
 
@@ -169,7 +165,9 @@
             {%- if col.is_number() %}
             cast(max({{ adapter.quote(col.name) }}) as varchar)
             {% else %}
+            {% if col.is_string() %}'"' || {% endif -%}
             max(cast({{ adapter.quote(col.name) }} as varchar))
+            {%- if col.is_string() %} || '"'{% endif %}
             {% endif %} as max_value
         {% endif %}
 
@@ -214,5 +212,10 @@
         select '{{ msg }}' as error    
     
     {% endfor %}
+            
+    )
+    select * from results 
+    {% if info_rate %}order by info_rate desc{% endif %}
+
 
 {% endmacro %}
